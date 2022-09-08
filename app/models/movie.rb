39a69c692666d96413069371601682d2b844d492
AnnaAyvazyan <ayvazyananna@yahoo.com>
2022-09-08 16:54:36 -0700

    commit tree 0649c2f3b94017a600bdc4b90691a16eec71dac1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,21 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title title
+        Movie.create(title: title)
+    end
 
-end
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        Movie.last
+    end
+    def self.movie_count
+        Movie.count
+    end
+    def self.find_movie_with_id id
+        Movie.find(id)
+    end
+    def self.find_movie_with_attributes title, release_date, director, lead, in_theaters
+        Movie.find_by(title:title, release_date:release_date, director:director, lead:lead, in_theaters:in_theaters)
+    end
+    end
